c3e8bbde511b0eacf41351de51fc5745e542b5e3
Abdallah-55 <112090570+Abdallah-55@users.noreply.github.com>
2024-06-19 14:49:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,9 @@ const persist = require('node-persist');
 const botConfigs = [
   {
     host: 'donutsmp.net',
-    username: 'abdoram999@gmail.com',
-    auth: 'microsoft'
-  },
-  
-  {
-    host: 'donutsmp.net',
-    username: 'yossefman123@gmail.com',
-    auth: 'microsoft'
-  },
-  
-  {
-    host: 'donutsmp.net',
     username: 'malekahmed2006@outlook.com',
     auth: 'microsoft'
-  },
-  
+  }
   /*{
     host: 'T2eelYaBoody.aternos.me',
     username: 'bot02',
